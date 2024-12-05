c1f9f5ec118de814e3a84d225031e5bc30b069bf
Vipin Kumar <vipinkumar@Vipins-Laptop.local>
2024-12-04 20:04:52 -0700

    google

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
     },
     {
         path:'',
-        component:FbLoginComponent
+        component:GoogleConnectComponent
     },
     {
         path:'google',
